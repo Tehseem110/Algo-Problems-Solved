16cfd20b1316a5574bbc9e1a38851b5bcb54c8d6
Tehseem Ahmed <Tehseem010@Gmail.com>
2025-12-30 17:11:52 +0530

    Time: 0 ms (100%), Space: 10.1 MB (10.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-          unordered_map<char, int> charCountS;
-            unordered_map<char, int> charCountT;
+          unordered_map<char, int> charCount;
 
-            if(s.length() != t.length()) {
-                return false; // If lengths differ, they cannot be anagrams
-            }
+          if(s.length() != t.length()){
+            return false;
+          }
 
-            for(char c : s) {
-                charCountS[c]++;
-            }
+          for(char c: s) charCount[c]++;
+          for(char c: t) charCount[c]--;
 
-            for(char c : t) {
-                charCountT[c]++;
-            }
-
-            bool isAnagram = true;
-
+          for(auto& pair: charCount ){
+            if(pair.second != 0){
+                return false;
             
-
-            // Compare the character counts
-
-            for(auto& pair : charCountS) {
-                char c = pair.first;
-                int count = pair.second;
-
-                if(charCountT[c] != count) {
-                    isAnagram = false;
-                    break; // If any character count doesn't match, they are not anagrams
-                }
             }
 
+          }
+           
             
-            return isAnagram;
+            return true;
     }
 };
