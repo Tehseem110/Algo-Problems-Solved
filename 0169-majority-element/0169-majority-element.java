9ec86599c05ed471ec0c92198a1b03d757c797c1
Tehseem Ahmed <Tehseem010@Gmail.com>
2026-01-01 16:52:03 +0530

    Time: 1 ms (99.83%), Space: 55.7 MB (45.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,32 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-           int mostOccuringElement = 0;
-      int mostOccuringElementCount= 0;
-      
-      HashMap<Integer,Integer> map = new HashMap<>();
-      
-      for(int i = 0 ; i < nums.length ; i++){
-          
-         
-          
-          if(map.containsKey(nums[i])){
-             map.put(nums[i],map.get(nums[i]) + 1);
+       
+       
+            int count = 0;
+       int currentLeader = 0;
+       
+       for(int n: nums){
+          if(count == 0){
+              count = 1;
+              currentLeader = n;
               
+          }else if(currentLeader == n){
+              count++;
           }else{
-               map.put(nums[i],1) ;
-          }
-         
-          if(map.get(nums[i]) > mostOccuringElementCount ){
-              mostOccuringElementCount = map.get(nums[i]);
-              mostOccuringElement = nums[i];
-              
-          }
-           if(mostOccuringElementCount > nums.length / 2 ){
-              break;
+              count--;
           }
           
-          
-      }
-      return mostOccuringElement;
+       }
+      
+       return currentLeader;
+
     }
 }
