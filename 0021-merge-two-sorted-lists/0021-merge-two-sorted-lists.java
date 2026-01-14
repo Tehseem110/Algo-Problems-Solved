a56b8f1bd54ecc44b833833c58659fabad49cd86
Tehseem Ahmed <Tehseem010@Gmail.com>
2026-01-14 12:42:39 +0530

    Time: 0 ms (100%), Space: 44.6 MB (11.2%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,80 +10,37 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        /// new Merged Sortet create new List to save sorted merged item from both list
-       
-       ListNode headList = new ListNode();
-       ListNode newList = headList;
-       
-      
-       
-       // iterate through both list append item to the list 
-       
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-       
-        while(temp1 != null || temp2 != null){
-            
-            
-            /// edge case for last element 
-            
-            // if list one exist and list 2 does not have items
-            if(temp1 != null && temp2 == null){
-                 System.out.println("List 1 e : " + temp1.val);
-                  if(newList == null){
-                      newList = new ListNode(temp1.val);
-                        headList = newList;
-                  }else{
-                      newList.next = new ListNode(temp1.val);
-                       newList = newList.next;
-                  }
-                 temp1 = temp1.next;
-                 continue;
-            }
-            
-            // if list two exist and list one does not have items
-             if(temp1 == null && temp2 != null){
-                 System.out.println("List 2 e : " + temp2.val);
-                 if(newList == null){
-                      newList = new ListNode(temp2.val);
-                        headList = newList;
-                  }else{
-                      newList.next = new ListNode(temp2.val);
-                       newList = newList.next;
-                  }
-                 temp2 = temp2.next;
-                   continue;
-            }
-            //  System.out.println("Comparing for :  temp1 " + temp1.val +  "-----" +  " temp2 " + temp2.val);
-            if(temp1.val < temp2.val){
-   
-                 System.out.println("List 1 : " + temp1.val);
-                  if(newList == null){
-                      newList = new ListNode(temp1.val);
-                      headList = newList;
-                  }else{
-                      newList.next = new ListNode(temp1.val);
-                      newList = newList.next;
-                  }
-                 temp1 = temp1.next;
-                
-            }else{
-                 if(newList == null){
-                      newList = new ListNode(temp2.val);
-                        headList = newList;
-                  }else{
-                      newList.next = new ListNode(temp2.val);
-                       newList = newList.next;
-                      
-                  }
-                 System.out.println("List 2 : " + temp2.val);
-                  temp2 = temp2.next;
-            }
-           
-       
-           
-        }
-        
-         return headList.next; 
+      ListNode dummy = new ListNode(0);   // Dummy head
+ListNode tail = dummy;
+
+ListNode temp1 = list1;
+ListNode temp2 = list2;
+
+while (temp1 != null && temp2 != null) {
+    if (temp1.val < temp2.val) {
+        tail.next = new ListNode(temp1.val);
+        temp1 = temp1.next;
+    } else {
+        tail.next = new ListNode(temp2.val);
+        temp2 = temp2.next;
+    }
+    tail = tail.next;
+}
+
+// Append remaining elements
+while (temp1 != null) {
+    tail.next = new ListNode(temp1.val);
+    temp1 = temp1.next;
+    tail = tail.next;
+}
+
+while (temp2 != null) {
+    tail.next = new ListNode(temp2.val);
+    temp2 = temp2.next;
+    tail = tail.next;
+}
+
+return dummy.next;
+
     }
 }
