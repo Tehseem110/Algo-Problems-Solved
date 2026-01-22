4f5aaff7eba98935277498a1d920a9a8c887047d
Tehseem Ahmed <Tehseem010@Gmail.com>
2026-01-22 15:52:10 +0530

    Time: 2 ms (66.15%), Space: 85.5 MB (14.45%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,30 +1,14 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
         
-         int trackNeg = 0;
-       int trackPos = 0;
-       
-       
-       
-       
-       
-       
-       for(int i = 0;i < nums.length - 1  ; i++){
-           if(nums[i] < nums[i + 1]){
-               trackNeg++;
-           }
-            if(nums[i] > nums[i + 1]){
-               trackPos++;
-           }
-       }
-      
-       
-      if(trackNeg == 0 || trackPos == 0 ){
-        
-          return true;
-      }else{
-         
-          return false;
-      }
+       int inc = 0;
+        int dec = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) inc++;
+            if (nums[i] > nums[i + 1]) dec++;
+        }
+
+        return inc == 0 || dec == 0;
     }
 }
